ba5b3d9cf97e897b67f8b13e53e09a3b3d493527
s-nashiro <nvmtest07@gmail.com>
2021-10-17 06:12:07 +0900

    q1-q4回答

--- task_org.rb
+++ task_org.rb
@@ -6,9 +6,6 @@ def q1
 
   # 以下に回答を記載
 
-  names << ["斎藤"]
-  p names
-
 end
 
 def q2
@@ -16,15 +13,13 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array = array1 + array2
-  puts array
+
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-  puts numbers.count(3)
 
 end
 
@@ -32,7 +27,6 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.reject!{|e| e == nil}
 
   # 以下は変更しないで下さい
   p sports
