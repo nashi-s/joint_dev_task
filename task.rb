5a4c60e8d6c314083df7b8a08ad8c589e4f8ff5c
s-nashiro <nvmtest07@gmail.com>
2021-10-19 21:24:47 +0900

    q7-q8修正

--- task.rb
+++ task.rb
@@ -59,7 +59,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array = array.map(&:to_i)
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -69,6 +69,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
@@ -80,6 +81,7 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  
 
 end
 
@@ -87,13 +89,15 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  
+  
 
 end
 
